08ad7a843a7e22925bd76c4468d2c73517899397
Islam <37441560+Islamjdallali@users.noreply.github.com>
2022-10-04 10:44:59 +0100

    Finished Basic collision
    try to make the actual position detection better.

--- Sphere to Sphere/Assets/Scripts/PlaneCollision.cs
+++ Sphere to Sphere/Assets/Scripts/PlaneCollision.cs
@@ -8,6 +8,7 @@ public class PlaneCollision : MonoBehaviour
     public Vector3 sphereStartPos;
 
     public Vector3 velocityVector;
+    public float velocityVectorMagnitude;
 
     float radius;
 
@@ -34,24 +35,6 @@ public class PlaneCollision : MonoBehaviour
         radius = bounds.extents.x;
 
         sphereStartPos = new Vector3(gameObject.GetComponent<Transform>().position.x, gameObject.GetComponent<Transform>().position.y, gameObject.GetComponent<Transform>().position.z);
-
-        //find the Vector between the sphere and the plane
-        SPVectorX = Mathf.Abs(planeTransform.position.x - transform.position.x);
-        SPVectorY = Mathf.Abs(planeTransform.position.y - transform.position.y);
-        SPVectorZ = Mathf.Abs(planeTransform.position.z - transform.position.z);
-
-        ABLength = Mathf.Sqrt((SPVectorX * SPVectorX) + (SPVectorY * SPVectorY) + (SPVectorZ * SPVectorZ));
-
-        //find the angle between the sphere vector and the plane
-        spherePlaneAngleZ = 90 - (Mathf.Acos(SPVectorZ / ABLength) * Mathf.Rad2Deg);
-        spherePlaneAngleX = 90 - (Mathf.Acos(SPVectorX / ABLength) * Mathf.Rad2Deg);
-        spherePlaneAngleY = 90 - (Mathf.Acos(SPVectorY / ABLength) * Mathf.Rad2Deg);
-
-        //this gives us the shortest distance betweent the sphere and the plane at its start position
-        startSpherePlaneDistance = Mathf.Sin(spherePlaneAngleY) * ABLength;
-
-        //this gives us the angle between the sphere vector and the startSpherePlaneDistance
-        vectorSpherePlaneDistanceAngleY = 180 - (90 + spherePlaneAngleY);
     }
 
     // Update is called once per frame
@@ -59,22 +42,32 @@ public class PlaneCollision : MonoBehaviour
     {
         this.gameObject.transform.Translate(velocityVector * Time.deltaTime);
 
-        float vCY = (startSpherePlaneDistance - radius) / Mathf.Cos(vectorSpherePlaneDistanceAngleY);
+        //find the Vector between the sphere and the plane
+        SPVectorX = Mathf.Abs(planeTransform.position.x - transform.position.x);
+        SPVectorY = Mathf.Abs(planeTransform.position.y - transform.position.y);
+        SPVectorZ = Mathf.Abs(planeTransform.position.z - transform.position.z);
+
+        velocityVectorMagnitude = Mathf.Sqrt((velocityVector.x * velocityVector.x) + (velocityVector.y * velocityVector.y) + (velocityVector.z * velocityVector.z));
+
+        ABLength = Mathf.Sqrt((SPVectorX * SPVectorX) + (SPVectorY * SPVectorY) + (SPVectorZ * SPVectorZ));
+
+        //find the angle between the sphere vector and the plane
+        spherePlaneAngleZ = (Mathf.Acos(SPVectorZ / ABLength));
+        spherePlaneAngleX = (Mathf.Acos(SPVectorX / ABLength));
+        spherePlaneAngleY = (Mathf.Acos(SPVectorY / ABLength));
+
+        float vCY = (SPVectorY - radius) / (Mathf.Cos(spherePlaneAngleY));
 
         //check to see if a collision is possible
-        if (vCY <= ABLength)
+        if (vCY <= velocityVectorMagnitude)
         {
-            float expectedYPos = (sphereStartPos.y) - (SPVectorY * (1/ABLength) * vCY);
+            Vector3 addedvCY = vCY * velocityVector / velocityVectorMagnitude;
+            float addedvCYMag = Mathf.Sqrt((addedvCY.x * addedvCY.x) + (addedvCY.y * addedvCY.y) + (addedvCY.z * addedvCY.z));
 
-            //Debug.Log("SPVectorY : " + SPVectorY);
-            //Debug.Log("AB Length : " + ABLength);
-            //Debug.Log("VCY : " + vCY);
-
-            Debug.Log("Expected Pos : " + expectedYPos);
-
-            if (transform.position.y <= expectedYPos)
+            if (addedvCYMag <= 0.01f)
             {
                 Debug.Log("Collided");
+                velocityVector = new Vector3(0,0,0);
             }
         }
     }
