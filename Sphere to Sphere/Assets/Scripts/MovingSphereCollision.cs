2abf975adcc0809dceb1ab093b232d439c13df4b
Islam <37441560+Islamjdallali@users.noreply.github.com>
2022-10-11 10:36:09 +0100

    found the vector difference and used the quadratic formula to find t
    t being the time in which both spheres would collide

--- Sphere to Sphere/Assets/Scripts/MovingSphereCollision.cs
+++ Sphere to Sphere/Assets/Scripts/MovingSphereCollision.cs
@@ -6,6 +6,8 @@ public class MovingSphereCollision : MonoBehaviour
 {
     public Transform otherSphere;
     public Vector3 velocityVector;
+    public Vector3 secondSphereVelocityVector;
+
     public Vector3 sphereStartPos;
     private Vector3 secondSphereStartPos;
 
@@ -30,6 +32,8 @@ public class MovingSphereCollision : MonoBehaviour
     {
         this.gameObject.transform.Translate(velocityVector * Time.deltaTime);
 
+        otherSphere.Translate(secondSphereVelocityVector * Time.deltaTime);
+
         //work out t, t is the interpolated time value, where t = 0 is the sphere's start pos and t = 1 is the sphere's end pos of their velocity vector
         float interpolationRatio = (float)elapsedFrames/interpolatedFrames;
         elapsedFrames = (elapsedFrames + 1) % (interpolatedFrames + 1);
@@ -38,8 +42,26 @@ public class MovingSphereCollision : MonoBehaviour
         if (interpolationRatio >= 1)
         {
             sphereStartPos = new Vector3(gameObject.GetComponent<Transform>().position.x, gameObject.GetComponent<Transform>().position.y, gameObject.GetComponent<Transform>().position.z);
+            secondSphereStartPos = new Vector3(otherSphere.position.x,otherSphere.position.y,otherSphere.position.z);
         }
 
+        float xPositionDifference = sphereStartPos.x - secondSphereStartPos.x;
+        float yPositionDifference = sphereStartPos.y - secondSphereStartPos.y;
+        float zPositionDifference = sphereStartPos.z - secondSphereStartPos.z;
+
+        float xVectorDifference = sphereStartPos.x - secondSphereStartPos.x;
+        float yVectorDifference = sphereStartPos.y - secondSphereStartPos.y;
+        float zVectorDifference = sphereStartPos.z - secondSphereStartPos.z;
+
+        float a = Mathf.Pow(xVectorDifference,2) + Mathf.Pow(yVectorDifference,2) + Mathf.Pow(zVectorDifference,2);
+        float b = (2 * xPositionDifference * xVectorDifference) + (2 * yPositionDifference * yVectorDifference) + (2 * zPositionDifference * zVectorDifference);
+        float c = Mathf.Pow(xPositionDifference,2) + Mathf.Pow(yPositionDifference,2) + Mathf.Pow(zPositionDifference,2) - Mathf.Pow(2 * radius,2);
+
+        float t = (-b + Mathf.Sqrt(Mathf.Pow(b,2) - (4 * a * c))/2 * a);
+        float t2 = (-b - Mathf.Sqrt(Mathf.Pow(b,2) - (4 * a * c))/2 * a);
+
+        Debug.Log("solution 1 : " + t);
+        Debug.Log("solution 2 : " + t2);
 
     }
 }
